71db5ab73f8b1c8de48d7fcd7506afbecfdceb7e
Corey <corey@notmeta.io>
2025-12-01 23:19:00 +0000

    setup

---
+++ aoc/__init__.py
@@ -0,0 +1,9 @@
+from abc import ABC
+
+
+class Aoc(ABC):
+    def part1(self, data: list[str]) -> str:
+        raise NotImplementedError()
+
+    def part2(self, data: list[str]) -> str:
+        raise NotImplementedError()
